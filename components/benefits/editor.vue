4a0210692734771864714f2d5b0ac289b05151ad
greaterpat <patrickobi1021@gmail.com>
2019-03-22 09:29:08 +0100

    refactored create benefit form

--- components/benefits/editor.vue
+++ components/benefits/editor.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="editor">
         <form class="form">
-            <div class="close-benefit"><a href="#" @click.prevent="$emit('cancelForm')"><i class="fa fa-window-close"></i></a></div>
+            <div class="create-close">
+                <button class="add-fields" @click.prevent="addItem">Add fields</button>
+                <a href="#" @click.prevent="$emit('cancelForm')"><i class="fa fa-window-close"></i></a>
+            </div>
             <div v-for="(item, i) in items">
                 <input class="input-benefit" v-model="item.name" placeholder="benefit">
                 <i class="fa fa-remove" @click="removeItem(i)"></i>
             </div>
-            <button @click.prevent="addItem">Add fields</button>
+            
 
             <div class="btn-benefits">
                 <button class="btn-cancel-benefits" @click.prevent="$emit('cancelForm')">Cancel</button>
@@ -14,6 +17,13 @@
                     <img v-if="isLoading" src="~/assets/images/loading.gif" alt="Elegant image">
                     <span v-else>{{ sendBtnTxt }}</span>
                 </button>
+            </div>
+            <div class="btn-benefits-mobile">
+                <button class="btn-submit-benefits" @click.prevent="submitBenefit">
+                    <img v-if="isLoading" src="~/assets/images/loading.gif" alt="Elegant image">
+                    <span v-else>{{ sendBtnTxt }}</span>
+                </button>
+                <button class="btn-cancel-benefits" @click.prevent="$emit('cancelForm')">Cancel</button>
             </div>
         </form>
     </div>
@@ -76,69 +86,112 @@
         display: grid;
         grid-template: 1fr / 1fr;
         border-radius: 5px;
-        /* width: 50%; */
     }
     .form{
         display: grid;
+        grid-template: 1fr / 1fr;
         grid-gap: 20px;
         padding: 20px;
-        background-color: rgb(211, 97, 4);
+        background-color: rgb(111, 185, 185);
         height: 1fr;
         border-radius: 5px;
-        /* width: 80%; */
+        border: 1px solid grey;
     }
-    button{
-        outline: none;
-    }
-    
     .btn-benefits{
         display: grid;
         grid-template-columns: 1fr 1fr;
-        justify-content: end;
-        align-items: end;
+        /* justify-content: end;
+        align-items: end; */
         grid-gap: 16px;
+        
+        font-size: 16px;
+        outline: none;
     }
     .btn-cancel-benefits{
         width: 170px;
         border-radius: 5px;
         border: 1px solid grey;
         height: 40px;
+        color: #fefefe;
+        transition: 0.6s ease-in;
+        background-color: rgb(245, 100, 100);
+    }
+    .btn-cancel-benefits:hover{
+        background-color: rgb(240, 59, 59);
     }
     .btn-submit-benefits{
         width: 170px;
         border-radius: 5px;
         border: 1px solid grey;
+        color: #fefefe;
         height: 40px;
+        background-color: #e7b83f;
+        transition: 0.6s ease-in;
     }
-    .close-benefit{
+    .btn-submit-benefits:hover{
+        background-color: #f58b13;
+    }
+    .create-close{
         display: grid;
-        justify-content: flex-end;
+        grid-template-columns: 1fr auto;
+        grid-gap: 20px;
     }
-    .close-benefit a:hover{
-        color: darkslategray;
+    .create-close a{
+        color: rgb(245, 100, 100);
+        transition: 0.8s ease-in;
+    }
+    .create-close a:hover{
+        color: rgb(240, 59, 59);
+    }
+    .add-fields{
+        height: 40px;
+        color: #3f3f3f;
+        border: 1px solid #fefefe;
+        border-radius: 5px;
+        outline: none;
+        background-color: #cecece;
+        transition: 0.8s ease-in;
+        font-size: 17px;
+    }
+    .add-fields:hover{
+        color: #fefefe;
+        background-color: #575757;
     }
     .input-benefit{
         height: 40px;
-        border: 10x solid grey;
+        border: 3px solid rgb(36, 136, 36);
         border-radius: 5px;
+        padding: 20px;
+    }
+    .btn-benefits-mobile{
+        display: none;
     }
 
     @media (max-width: 767px) {
-        .editor{
-            width: 20%;
-        }
         .form{
+            padding: 15px;
         }
         .btn-benefits{
+            display: none;
         }
         .btn-cancel-benefits{
             height: 30px;
+            font-size: 14px;
         }
         .btn-submit-benefits{
             height: 30px;
+            font-size: 14px;
+        }
+        .create-close{
+            grid-gap: 12px;
         }
         .input-benefit{
             height: 30px;
+        }
+        .btn-benefits-mobile{
+            display: grid;
+            grid-template-rows: 1fr 1fr;
+            grid-gap: 12px;
         }
     }
 </style>

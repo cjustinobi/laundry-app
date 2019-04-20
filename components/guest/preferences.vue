b555ec4e8bce62e336dc4a007596fede237bb754
greaterpat <patrickobi1021@gmail.com>
2019-04-21 00:59:01 +0100

    added preference toggle switch

--- components/guest/preferences.vue
+++ components/guest/preferences.vue
@@ -2,16 +2,48 @@
     <div class="edit-preferences" :style="myStyle">
         <div class="preferences-wrapper">
             <div class="preferences-list">
-                <label for=""><span class="preferences-head">Preferences</span> <br>
-                    <input type="radio" class="starch" name="" id="" checked> Starch <br>
-                    <input type="radio" name="" id=""> Light Starch <br>
-                    <input type="radio" name="" id=""> No Starch 
-                </label>
-                <label for=""><span class="preferences-head">Shirt Laundry Preferences</span> <br>
-                    <input type="radio" class="hung" name="" id="" checked> Hung <br>
-                    <input type="radio" name="" id=""> Fold 
-                </label>
+                <h3>Starch Preferences</h3>
+                <div class="preferences-head">
+                    <p>Starch</p>
+                    <span>
+                        <i class="fa fa-toggle-off" v-if="!toggleOn" @click.prevent="toggleSwitch()"></i>
+                        <i class="fa fa-toggle-on" v-if="toggleOn" @click.prevent="toggleSwitch()"></i>
+                    </span>
+                </div>
+                <div class="preferences-head">
+                    <p>Light Starch</p>
+                    <span>
+                        <i class="fa fa-toggle-off" v-if="!toggleOn" @click.prevent="toggleSwitch()"></i>
+                        <i class="fa fa-toggle-on" v-if="toggleOn" @click.prevent="toggleSwitch()"></i>
+                    </span>
+                </div>
+                <div class="preferences-head">
+                    <p>No Starch</p>
+                    <span>
+                        <i class="fa fa-toggle-off" v-if="!toggleOn" @click.prevent="toggleSwitch()"></i>
+                        <i class="fa fa-toggle-on" v-if="toggleOn" @click.prevent="toggleSwitch()"></i>
+                    </span>
+                </div>
             </div>
+
+            <div class="shirt-laundry-preferences">
+                <h3>Shirt Laundry Preferences</h3>
+                <div class="preferences-head">
+                    <p>Hung</p>
+                    <span>
+                        <i class="fa fa-toggle-off" v-if="!toggleOn" @click.prevent="toggleSwitch()"></i>
+                        <i class="fa fa-toggle-on" v-if="toggleOn" @click.prevent="toggleSwitch()"></i>
+                    </span>
+                </div>
+                <div class="preferences-head">
+                    <p>Fold</p>
+                    <span>
+                        <i class="fa fa-toggle-off" v-if="!toggleOn" @click.prevent="toggleSwitch()"></i>
+                        <i class="fa fa-toggle-on" v-if="toggleOn" @click.prevent="toggleSwitch()"></i>
+                    </span>
+                </div>
+            </div>
+
             <div class="update-preferences">
                 <button @click.prevent="signUp" class="preferences-button">
                     Change Preferences
@@ -30,7 +62,13 @@ export default {
         return {
             myStyle: {
                 backgroundColor: "#f0faff"
-            }
+            },
+            toggleOn: false
+        }
+    },
+    methods: {
+        toggleSwitch() {
+            this.toggleOn ? this.toggleOn = false : this.toggleOn = true
         }
     },
     mounted() {
@@ -52,26 +90,34 @@ export default {
     }
     .preferences-wrapper{
         display: grid;
-        grid-template-rows: 200px 90px;
+        grid-template-rows: 110px 90px 90px;
         background-color: #fefefe;
         margin: 0 160px 40px 160px;
-        padding: 80px 70px 0 70px;
+        padding: 70px 70px 0 70px;
         box-shadow: 5px 5px 15px grey;
-        grid-gap: 10px;
+        grid-gap: 18px;
         height: 400px;
         color: #114e9e;
     }
     .preferences-list{
         display: grid;
-        grid-gap: 20px;
+        grid-gap: 5px;
+        grid-template-rows: 30px repeat(3, 20px);
     }
     .preferences-head{
-        font-size: 22px;
+        display: grid;
+        grid-template-columns: 220px 50px;
+        grid-gap: 50px;
+    }
+    .shirt-laundry-preferences{
+        display: grid;
+        grid-gap: 5px;
+        grid-template-rows: 30px repeat(2, 20px);
     }
     .update-preferences{
         display: grid;
         justify-content: center;
-        margin: 20px 0;
+        margin: 15px 0;
     }
     .preferences-button{
         height: 50px;

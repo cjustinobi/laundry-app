8795a18008f46e5675d0fe1a61070e397cfb1c99
Cejowisz <cjustinobi@gmail.com>
2019-04-26 19:55:05 +0100

    renamed more files

--- plugins/notification.js
+++ plugins/notification.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
-import Notification from '../components/shared/Notification.vue'
+import Notification from '../components/shared/notification.vue'
 
 Vue.component('Notification', Notification)

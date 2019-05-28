fe19d5535e6836d06fab37be732f9a2d68df3738
greaterpat <patrickobi1021@gmail.com>
2019-05-28 15:54:56 +0100

    refactored package list

--- pages/cart.vue
+++ pages/cart.vue
@@ -43,13 +43,48 @@ export default {
         color: #fefefe;
         transition: 0.8s ease-in;
         font-size: 14px;
-        /* text-align: center; */
         display: grid;
         align-items: center;
         justify-items: center;
+        animation: bounce 0.95s infinite;
+        -webkit-animation: bounce 0.95s infinite;
     }
     .continue-btn:hover{
         background-color: #e7b83f;
+    }
+    @keyframes bounce {
+        0% {
+            /* // transform: scale(1,1) translate(0px, 0px); */
+        }
+        
+        30%{
+            transform: scale(1,0.8) translate(0px, 10px); 
+        }
+
+        75%{
+            /* // transform: scale(1,1.1) translate(0px, -25px);  */
+        }
+        
+        100% {
+            /* // transform: scale(1,1) translate(0px, 0px); */
+        }
+    }
+    @-webkit-keyframes bounce {
+        0% {
+            /* // transform: scale(1,1) translate(0px, 0px); */
+        }
+        
+        30%{
+            transform: scale(1,0.8) translate(0px, 10px); 
+        }
+
+        75%{
+            /* // transform: scale(1,1.1) translate(0px, -25px);  */
+        }
+        
+        100% {
+            /* // transform: scale(1,1) translate(0px, 0px); */
+        }
     }
 
      @media (max-width: 1110px) {

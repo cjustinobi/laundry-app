55f16376dfe49b38f5b1fbf60dc3e1424b22794f
Cejowisz <cjustinobi@gmail.com>
2019-06-01 18:23:13 +0100

    deleted comment from axios plugin

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,9 +3,5 @@ export default function ({ $axios, store }) {
         config.headers.common['access-token'] = store.state.auth.token
         config.headers.common['user_type'] = store.state.auth.user ? store.state.auth.user.user_type : null
         config.headers.common['user_id'] = store.state.auth.user ? store.state.auth.user.id : null
-        // config.headers.common['token-type'] = store.state.user.headers.token_type
-        // config.headers.common['client'] = store.state.user.headers.client
-        // config.headers.common['expiry'] = store.state.user.headers.expiry
-        // config.headers.common['uid'] = store.state.user.headers.uid
     })
 }

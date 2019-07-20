996cbfbc7bed889f29733d6bcc859a1496ef295f
cjustinobi <cjustinobi@gmail.com>
2019-07-20 12:29:08 +0100

    refactoring package list

--- components/packages/packagesList.vue
+++ components/packages/packagesList.vue
@@ -5,7 +5,7 @@
             <div class="pack-item" v-for="(plan, i) in plans" :key="i">
                 <actions
                         v-if="user !== undefined && user.user_type == 3"
-                    	:class="[{'ellipsis-product': productList}, {'ellipsis-package': !productList}]"
+                        :class="[{'ellipsis-product': productList}, {'ellipsis-package': !productList}]"
                         @editItem="editItem"
                         @removeItem="removeItem"
                         :itemId="plan.id"
@@ -13,25 +13,24 @@
                 </actions>
                 <div class="name">
                     <img class="elegant-image" src="~assets/images/EL_logo_3.png" alt="Elegant Laundry">
-                    <h1>{{ plan.name }}</h1>
+                    <h1 class="gray-txt">{{ plan.name }}</h1>
                 </div>
                 <div class="price">
                     <h1>&#8358;{{ currency.format(plan.price) }}/<span class="gray-txt">month</span></h1>
                 </div>
                 <div class="wash-container">
                     <div class="wash-menu" v-for="(benefit, i) in plan.benefits" :key="i">
-                        <i class="fa fa-check"></i>
-                        <li>{{ benefit.name }}</li>
+                        <p>{{ benefit.name }}</p>
                     </div>
                 </div>
                 <form v-if="user !== undefined && user.user_type !== 3" method="get" id="sub-btn">
                     <button class="subscribe" @click.prevent="subscribe" type="submit">SUBSCRIBE</button>
                 </form>
             </div>
-    </div>
+        </div>
         <div :class="{'backdrop' : showForm}">
             <div :class="[{'show-form': showForm, 'hide-form': !showForm}]">
-                <editor :editDetail="editDetail" @cancelForm="showForm = false"></editor>
+                <Editor :editDetail="editDetail" @cancelForm="showForm = false"/>
             </div>
         </div>
     </div>
@@ -66,10 +65,10 @@
             removeItem(i) {
                 this.$store.dispatch('plans/removeItem', i)
             },
-             editItem(i) {
-                 this.showForm = true;
-                 this.editDetail = this.plans.find(plan => plan.id === i)
-             },
+            editItem(i) {
+                this.showForm = true;
+                this.editDetail = this.plans.find(plan => plan.id === i)
+            },
             subscribe() {
                 this.$root.$emit("package", 2000);
                 this.$router.push({ path: "/volunteer" });
@@ -98,8 +97,7 @@
         text-align: center;
         margin-top: 48px;
     }
-    .pkg-txt::after
-    {
+    .pkg-txt::after{
         position: absolute;
         left: calc(50% - 35px);
         display: block;
@@ -111,23 +109,19 @@
     .packages{
         position: relative;
         display: grid;
-        grid-template: auto / repeat(auto-fit, minmax(150px, 350px));
+        grid-template-columns: repeat(auto-fit, minmax(150px, 350px));
         justify-content: center;
         justify-items: center;
         grid-gap: 40px;
-        margin: 40px 0;
-        padding: 20px 0;
+        /*margin: 40px 0;
+        padding: 20px 0;*/
     }
     .pack-item{
         position: relative;
-        border: 1px solid #b2d2e4;
-        border-radius: 10px;
-        display: grid;
+        border: 1px solid #dcdcdc;
         min-height: 450px;
         background-color: #fefefe;
         transition: 0.7s ease-in;
-        grid-gap: 10px;
-        grid-template-rows: 120px 60px 187px 40px;
     }
     .packages .pack-item:nth-child(1){
         border-top: 7px solid rgb(247, 174, 241);
@@ -167,19 +161,12 @@
     }
     .wash-container{
         display: grid;
-        grid-template-rows: repeat(auto-fit, 40px);
     }
     .wash-menu{
         display: grid;
-        grid-template-columns: 30px 1fr;
-        color: #728691;
-        font-size: 16px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        padding: 0 15px;
+
     }
-    .wash-menu li{
-        list-style-type: none;
-    }
+
     .elegant-image{
         width: 40px;
         padding-top: 6px;
@@ -187,11 +174,8 @@
     .name{
         display: grid;
         grid-template-columns: 60px 250px;
-        align-items: center;
         justify-items: center;
-        font-family: Courier;
         font-size: 18px;
-        color: #728691;
         grid-gap: 5px;
         padding: 5px;
     }
@@ -199,19 +183,13 @@
         margin-bottom: 0px;
         color: rgb(255, 255, 255);
         background-color: #2f5163;
-        height: 60px;
         width: 100%;
         display: grid;
         justify-content: center;
-        align-content: center;
     }
-    .month{
-        font-size: 14px;
-        color: #b0b9c5;
-    }
+
     #sub-btn{
         display: grid;
-        align-items: center;
         justify-self: center;
     }
     .subscribe{
@@ -238,7 +216,6 @@
     @media (max-width: 767px) {
         .packages{
             grid-template: auto / repeat(auto-fit, minmax(150px, 300px));
-            grid-gap: 30px;
         }
         .wash-menu{
             font-size: 14px;
@@ -252,7 +229,6 @@
         .name{
             grid-template-columns: 60px 190px;
             font-size: 14px;
-            /* max-width: 310px; */
         }
         .price h1{
             font-size: 16px;
